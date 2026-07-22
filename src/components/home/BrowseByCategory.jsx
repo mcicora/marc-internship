3cca6298477768159c4f585cef45c00983d85cab
Marc Cicora <mcicora@Marcs-Air.attlocal.net>
2026-07-22 11:25:09 -0400

    Added animations

--- src/components/home/BrowseByCategory.jsx
+++ src/components/home/BrowseByCategory.jsx
@@ -12,38 +12,75 @@ const BrowseByCategory = () => {
               <div className="small-border bg-color-2"></div>
             </div>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-offset="0"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-image"></i>
               <span>Art</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-delay="100"
+            data-aos-offset="0"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-music"></i>
               <span>Music</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link
+              to="/explore"
+              className="icon-box style-2 rounded"
+              data-aos="fade-zoom-in"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="200"
+              data-aos-offset="0"
+            >
               <i className="fa fa-search"></i>
               <span>Domain Names</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-delay="300"
+            data-aos-offset="0"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-globe"></i>
               <span>Virtual Worlds</span>
             </Link>
           </div>
-          <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
+          <div
+            className="col-md-2 col-sm-4 col-6 mb-sm-30"
+            data-aos="fade-zoom-in"
+            data-aos-easing="ease-in-back"
+            data-aos-delay="400"
+            data-aos-offset="0"
+          >
             <Link to="/explore" className="icon-box style-2 rounded">
               <i className="fa fa-vcard"></i>
               <span>Trading Cards</span>
             </Link>
           </div>
           <div className="col-md-2 col-sm-4 col-6 mb-sm-30">
-            <Link to="/explore" className="icon-box style-2 rounded">
+            <Link
+              to="/explore"
+              className="icon-box style-2 rounded"
+              data-aos="fade-zoom-in"
+              data-aos-easing="ease-in-back"
+              data-aos-delay="500"
+              data-aos-offset="0"
+            >
               <i className="fa fa-th"></i>
               <span>Collectibles</span>
             </Link>
--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -21,7 +21,7 @@ const Explore = () => {
 
   useEffect(() => {
     window.scrollTo(0, 0);
-    // Does this work?
+    // Any comment
   }, []);
 
   useEffect(() => {
